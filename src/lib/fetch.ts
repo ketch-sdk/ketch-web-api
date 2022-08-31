39b006c928965e716cc5370fa65faccf9dd4d343
keepforever <briancilenti@gmail.com>
2022-08-31 13:01:12 -0400

    chore: add code style config, run prettier, fix errors

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,3 +1,3 @@
-export default (url: string, fetchOptions: RequestInit): Promise<any> => {
-  return fetch(url, fetchOptions).then((resp: Response) => resp.json());
-};
+export default async (url: string, fetchOptions: RequestInit): Promise<any> => {
+  return fetch(url, fetchOptions).then((resp: Response) => resp.json())
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,10 @@
-jest.mock('../src/lib/fetch');
+/* eslint-disable jest/valid-expect */
+jest.mock('../src/lib/fetch')
 
-import fetch from "../src/lib/fetch";
-import {mocked} from 'ts-jest/utils';
+import fetch from '../src/lib/fetch'
+import { mocked } from 'ts-jest/utils'
 
-const mockFetch = mocked(fetch);
+const mockFetch = mocked(fetch)
 
 import {
   getBootstrapConfiguration,
@@ -11,10 +12,10 @@ import {
   getFullConfiguration,
   getLocation,
   invokeRight,
-  setConsent
-} from "../src";
+  setConsent,
+} from '../src'
 
-const baseUrl = 'https://global.ketchcdn.com/web/v1';
+const baseUrl = 'https://global.ketchcdn.com/web/v1'
 
 describe('@ketch-com/ketch-web-api', () => {
   describe('GetLocation', () => {
@@ -23,168 +24,178 @@ describe('@ketch-com/ketch-web-api', () => {
         ip: '1.2.3.5',
         countryCode: 'US',
         regionCode: 'CA',
-      };
-      mockFetch.mockResolvedValue(v);
+      }
+      mockFetch.mockResolvedValue(v)
 
-      expect(getLocation(baseUrl)).resolves.toBe(v);
+      expect(getLocation(baseUrl)).resolves.toBe(v)
 
-      expect(mockFetch).toBeCalled();
-    });
-  });
+      expect(mockFetch).toHaveBeenCalled()
+    })
+  })
 
   describe('GetBootstrapConfiguration', () => {
     it('calls service', () => {
       const v = {
         language: 'en-US',
         regulations: ['gdpr'],
-      };
-      mockFetch.mockResolvedValue(v);
+      }
+      mockFetch.mockResolvedValue(v)
 
-      expect(getBootstrapConfiguration(baseUrl, {
-        organizationCode: 'switchbitcorp',
-        appCode: 'switchbit',
-      })).resolves.toBe(v);
+      expect(
+        getBootstrapConfiguration(baseUrl, {
+          organizationCode: 'switchbitcorp',
+          propertyCode: 'foo',
+        }),
+      ).resolves.toBe(v)
 
-      expect(mockFetch).toBeCalled();
-    });
-  });
+      expect(mockFetch).toHaveBeenCalled()
+    })
+  })
 
   describe('GetFullConfiguration', () => {
     it('calls service', () => {
       const v = {
         language: 'en-US',
         regulations: ['gdpr'],
-      };
-      mockFetch.mockResolvedValue(v);
+      }
+      mockFetch.mockResolvedValue(v)
 
-      expect(getFullConfiguration(baseUrl,{
-        organizationCode: 'switchbitcorp',
-        appCode: 'switchbit',
-        envCode: 'production',
-        hash: '8913461971881236311',
-        deploymentID: '',
-        policyScopeCode: 'default',
-        languageCode: 'en-US',
-      })).resolves.toBe(v);
+      expect(
+        getFullConfiguration(baseUrl, {
+          languageCode: 'en-US',
+          hash: '8913461971881236311',
+          organizationCode: 'switchbitcorp',
+          propertyCode: 'foo',
+          environmentCode: 'foo',
+          deploymentID: '',
+          jurisdictionCode: 'foo',
+        }),
+      ).resolves.toBe(v)
 
-      expect(mockFetch).toBeCalled();
-    });
-  });
+      expect(mockFetch).toHaveBeenCalled()
+    })
+  })
 
   describe('GetConsent', () => {
     it('calls service', () => {
       const v = {
-        'purposes': {
-          'coreprodserv': {
-            'allowed': 'true',
-          },
-        },
-      };
-      mockFetch.mockResolvedValue(v);
-
-      expect(getConsent(baseUrl, {
-        organizationCode: 'switchbitcorp',
-        propertyCode: 'switchbit',
-        environmentCode: 'production',
-        identities: {
-          'swb_switchbit': '2I0tgfvRzAyP7A9ma7Eqo6'
-        },
-        jurisdictionCode: 'default',
         purposes: {
-          'coreprodserv': {
-            'legalBasisCode': 'disclosure',
-          },
-          'prodservenhance': {
-            'legalBasisCode': 'disclosure',
-          },
-          'hschat': {
-            'legalBasisCode': 'disclosure',
-          },
-          'bizsiteanalytics': {
-            'legalBasisCode': 'disclosure',
-          },
-          'persads': {
-            'legalBasisCode': 'disclosure',
-          },
-          'personzation':{
-            'legalBasisCode': 'disclosure',
+          coreprodserv: {
+            allowed: 'true',
           },
         },
-      })).resolves.toBe(v);
+      }
+      mockFetch.mockResolvedValue(v)
 
-      expect(mockFetch).toBeCalled();
-    });
-  });
+      expect(
+        getConsent(baseUrl, {
+          organizationCode: 'switchbitcorp',
+          propertyCode: 'switchbit',
+          environmentCode: 'production',
+          identities: {
+            swb_switchbit: '2I0tgfvRzAyP7A9ma7Eqo6',
+          },
+          jurisdictionCode: 'default',
+          purposes: {
+            coreprodserv: {
+              legalBasisCode: 'disclosure',
+            },
+            prodservenhance: {
+              legalBasisCode: 'disclosure',
+            },
+            hschat: {
+              legalBasisCode: 'disclosure',
+            },
+            bizsiteanalytics: {
+              legalBasisCode: 'disclosure',
+            },
+            persads: {
+              legalBasisCode: 'disclosure',
+            },
+            personzation: {
+              legalBasisCode: 'disclosure',
+            },
+          },
+        }),
+      ).resolves.toBe(v)
+
+      expect(mockFetch).toHaveBeenCalled()
+    })
+  })
 
   describe('SetConsent', () => {
     it('calls service', () => {
-      mockFetch.mockResolvedValue(void(0));
+      mockFetch.mockResolvedValue(void 0)
 
-      expect(setConsent(baseUrl, {
-        organizationCode: 'switchbitcorp',
-        propertyCode: 'switchbit',
-        environmentCode: 'production',
-        identities: {
-          'swb_switchbit': '2I0tgfvRzAyP7A9ma7Eqo6',
-        },
-        jurisdictionCode: '',
-        purposes: {
-          "coreprodserv":{
-            "allowed": 'true',
-            "legalBasisCode": 'legitimateinterest',
+      expect(
+        setConsent(baseUrl, {
+          organizationCode: 'switchbitcorp',
+          propertyCode: 'switchbit',
+          environmentCode: 'production',
+          identities: {
+            swb_switchbit: '2I0tgfvRzAyP7A9ma7Eqo6',
           },
-          "prodservenhance":{
-            "allowed": 'true',
-            "legalBasisCode": 'consent_optin',
+          jurisdictionCode: '',
+          purposes: {
+            coreprodserv: {
+              allowed: 'true',
+              legalBasisCode: 'legitimateinterest',
+            },
+            prodservenhance: {
+              allowed: 'true',
+              legalBasisCode: 'consent_optin',
+            },
+            hschat: {
+              allowed: 'false',
+              legalBasisCode: 'consent_optin',
+            },
+            bizsiteanalytics: {
+              allowed: 'false',
+              legalBasisCode: 'consent_optin',
+            },
+            persads: {
+              allowed: 'false',
+              legalBasisCode: 'consent_optin',
+            },
+            personzation: {
+              allowed: 'false',
+              legalBasisCode: 'consent_optin',
+            },
           },
-          "hschat":{
-            "allowed": 'false',
-            "legalBasisCode": 'consent_optin',
-          },
-          "bizsiteanalytics":{
-            "allowed": 'false',
-            "legalBasisCode": 'consent_optin',
-          },
-          "persads":{
-            "allowed": 'false',
-            "legalBasisCode": 'consent_optin',
-          },
-          "personzation":{
-            "allowed": 'false',
-            "legalBasisCode": 'consent_optin',
-          }
-        },
-        migrationOption: 1,
-      })).resolves.toBe(void(0));
+          migrationOption: 1,
+        }),
+      ).resolves.toBe(void 0)
 
-      expect(mockFetch).toBeCalled();
-    });
-  });
+      expect(mockFetch).toHaveBeenCalled()
+    })
+  })
 
   describe('InvokeRight', () => {
     it('calls service', () => {
-      mockFetch.mockResolvedValue(void(0));
+      mockFetch.mockResolvedValue(void 0)
 
-      expect(invokeRight(baseUrl, {
-        organizationCode: 'switchbitcorp',
-        propertyCode: 'switchbit',
-        environmentCode: 'production',
-        identities: {
-          'swb_switchbit': '2I0tgfvRzAyP7A9ma7Eqo6',
-        },
-        jurisdictionCode: 'gdpreea',
-        rightCodes: ['gdpr_portability'],
-        user: {
-          email:'ignore.me@ketch.com',
-          first: 'Just"',
-          last: 'Testing',
-          country: 'FR',
-          stateRegion: "France",
-          description: "Ignore me"
-        }
-      })).resolves.toBe(void(0));
+      expect(
+        invokeRight(baseUrl, {
+          organizationCode: 'switchbitcorp',
+          propertyCode: 'switchbit',
+          environmentCode: 'production',
+          identities: {
+            swb_switchbit: '2I0tgfvRzAyP7A9ma7Eqo6',
+          },
+          jurisdictionCode: 'gdpreea',
+          rightCode: 'gdpr_portability',
+          user: {
+            email: 'ignore.me@ketch.com',
+            first: 'Just"',
+            last: 'Testing',
+            country: 'FR',
+            stateRegion: 'France',
+            description: 'Ignore me',
+          },
+        }),
+      ).resolves.toBe(void 0)
 
-      expect(mockFetch).toBeCalled();
-    });
-  });
-});
+      expect(mockFetch).toHaveBeenCalled()
+    })
+  })
+})
