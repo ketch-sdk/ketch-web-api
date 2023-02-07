2653b7a491f16aca5736456693f8b75ef1bb7b4a
Seth Yates <29878+sethyates@users.noreply.github.com>
2023-02-07 13:47:19 -0800

    chore: upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -51,9 +51,7 @@ module.exports = {
   // globalTeardown: null,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    _VERSION: true,
-  },
+  globals: {},
 
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: ['node_modules'],
